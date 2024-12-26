b05cd4c523255cd647131435e2e685a741c56d6c
farhaghallab3 <farhagalab74@gmail.com>
2024-12-26 23:07:14 +0200

    init project frontend react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Login from "./pages/Login";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 import Tasks from "./pages/Tasks";
 import CompletedTasks from "./pages/CompletedTasks";
-import Signup from "./pages/Signup";
 
 const App = () => {
   return (
     <Router>
-      <div>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/tasks" element={<Tasks />} />
-          <Route path="/completed" element={<CompletedTasks />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/tasks" element={<Tasks />} />
+        <Route path="/completed" element={<CompletedTasks />} />
+      </Routes>
     </Router>
   );
 };
